39cc6c191bfbf7b816f8c91cb6efafb3f94d6904
yogo254 <yogo254@yahoo.com>
2019-06-11 21:15:08 +0300

    adding descriptive service for perfomance of descriptive analytics on spark framework and integrate with kafka streams and sink

---
+++ descriptive-analytics-service/src/main/java/com/yogo/descriptiveanalyticsservice/DescriptiveAnalyticsServiceApplication.java
@@ -0,0 +1,15 @@
+package com.yogo.descriptiveanalyticsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DescriptiveAnalyticsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescriptiveAnalyticsServiceApplication.class, args);
+	}
+
+}
