913e09778ebac4804694b7a6b3e500e0119e7a85
yogo254 <yogo254@yahoo.com>
2019-06-27 11:59:26 +0300

    cleaning up the repo

--- mysql-tran-service/src/main/java/com/yogo/repo/CustomerOrderRepo.java
+++
@@ -1,8 +0,0 @@
-package com.yogo.mysqltranservice.repo;
-
-import com.yogo.domain.CustomerOrder;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface CustomerOrderRepo extends CrudRepository<CustomerOrder,String> {
-}
--- mysql-tran-service/src/main/java/com/yogo/repo/OrderItemRepo.java
+++
@@ -1,8 +0,0 @@
-package com.yogo.mysqltranservice.repo;
-
-import com.yogo.domain.OrderItem;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface OrderItemRepo extends CrudRepository<OrderItem,String> {
-}
