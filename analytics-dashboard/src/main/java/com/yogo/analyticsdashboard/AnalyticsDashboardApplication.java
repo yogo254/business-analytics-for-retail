6b1e2eb8fdc4cf1098853c8d5b62dbb6a9e36a84
yogo254 <yogo254@yahoo.com>
2019-06-27 12:04:13 +0300

    adding javafx module for data viz

---
+++ analytics-dashboard/src/main/java/com/yogo/analyticsdashboard/AnalyticsDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.yogo.analyticsdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyticsDashboardApplication.class, args);
+    }
+
+}
